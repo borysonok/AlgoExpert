45ec15c92ea0d0b464573cf9306c385c6676ae9c
Oleksii Borysonok <alex.borysonok@gmail.com>
2018-10-11 20:04:54 -0400

    two-numbers second solution

--- two-number-sum-refresh.js
+++ two-number-sum-refresh.js
@@ -1,4 +1,5 @@
-//First brute-forse solution:
+// 1)
+// First brute-forse solution: O(n^2)-time | O(1)-space
 //
 function twoNumberSum(array, targetSum) {
 	for(let i=0; i<array.length-1; i++){
@@ -10,3 +11,18 @@ function twoNumberSum(array, targetSum) {
 	}
   return[];
 }
+
+// 2)
+// Using the hashing: O(n)-time | O(n)-space.
+//
+let num_Ys = {};
+	for(let i = 0; i < array.length; i++){
+		let x = array[i];
+		let y = targetSum - x;
+		if(num_Ys[y] !== undefined){
+			return [x, y].sort((a,b) => a-b);
+		} else {
+			num_Ys[x] = x;
+		}
+	}
+	return[];
