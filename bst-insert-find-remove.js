e98d9ad3e994c64c26bdc0de29f5f8e4a684ede2
Oleksii Borysonok <alex.borysonok@gmail.com>
2018-10-19 20:40:19 -0400

    bst-comtains

--- bst-insert-find-remove.js
+++ bst-insert-find-remove.js
@@ -5,17 +5,21 @@ class BST {
         this.right = null;
     }
 
+    // Avarage O(log(n))-time | O(1)-space
+    // Worst O(n)-time | O(1)-space
     insert(value) {
         let current = this;
         while (true) {
-            if (value < current.value) { // go left...
+            if (value < current.value) {
+                // go left...
                 if (current.left === null) {
                     current.left = new BST(value);
                     break;
                 } else {
                     current = current.left;
                 }
-            } else { // go right...
+            } else {
+                // go right...
                 if (current.right === null) {
                     current.right = new BST(value);
                     break;
@@ -27,12 +31,23 @@ class BST {
         return this;
     }
 
+    // Avarage O(log(n))-time | O(1)-space
+    // Worst O(n)-time | O(1)-space
     contains(value) {
-
+        let current = this;
+        while (current) {
+            if (value === current.value) {
+                return true;
+            } else if (value < current.value) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return false;
     }
 
     remove(value) {
-
         return this;
     }
 }
